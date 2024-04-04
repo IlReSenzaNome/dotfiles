c856a98522cb9d375ba1a140b8da444e2b7b00dc
IlReSenzaNome <alexisivas3@gmail.com>
2024-04-03 21:52:00 -0500

    add all config custom window manger

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Select wallpaper
+feh --bg-scale ~/.config/qtile/Wallpapers/wallpaper.jpg
+# Add this script to your wm startup file.
+
+DIR="$HOME/.config/polybar/"
+
+# Terminate already running bar instances
+killall -q polybar
+killall mpd 
+killall picom
+killall dunst
+killall daemon
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch the bar
+polybar -q power -c "$DIR"/config.ini &
+polybar -q clock -c "$DIR"/config.ini &
+polybar -q spotify -c "$DIR"/config.ini &
+polybar -q workspaces -c "$DIR"/config.ini &
+polybar -q sound -c "$DIR"/config.ini & 
+polybar -q settings -c "$DIR"/config.ini &
+polybar -q wifi -c "$DIR"/config.ini &
+# Play Reproductor
+mpd 
+# Shadows
+picom &
+# notifications
+dunst &
+# widgets
+daemon
+
+
